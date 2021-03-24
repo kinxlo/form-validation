ebc9eaf9905e40570cc79796eeafaaea591b297a
kingsley <kinxly@gmail.com>
2021-03-24 19:39:00 +0100

    validation 97%

--- js/main.js
+++ js/main.js
@@ -1,128 +1,129 @@
 // TERGETED ELEMENTS ===============================
+// target the html elements
+const form = document.querySelector('form') //the form element
+const formSections = form.querySelectorAll('section') //the form sections elements...returns a nodelist
+const submitButton = document.querySelector('.submit-btn') //the 'next' button
 
-const inputsDiv = document.querySelectorAll('.input-container') //collections of inputs field container
-const inputs = document.querySelectorAll('input') //collections of inputs
-const form = document.querySelector('form')
-const email = document.querySelector('#email')
-const emailErrorMessage = document.querySelector('.cc-email-error')
-const userName = document.querySelector('#name')
-const userNameErrorMessage = document.querySelector('.cc-name-error')
-const password = document.querySelector('#password')
-const submitButton = document.querySelector('.submit-btn')
+// FORM VALIDATION =================
+//loop through all the sections
+formSections.forEach((section) => {
+  let placeHolder //declear a variable to hold the placeholder element
+  const inputs = section.querySelectorAll('input') //target the input element as a nodelist or collection
 
-// ======== ANIMATED PLACEHOLDER =================
+  //again, loop through the input collection
+  inputs.forEach((input) => {
+    //  for each input, listen for the focus event
+    input.addEventListener('focus', (event) => {
+      //assign the placeholder element to the place holder varable decleared above
+      placeHolder = event.target.parentNode.children[1]
+      //if the input element has an empty value and has focus
+      if (input.value == '') {
+        //change the position and color of the placeholder
+        placeHolder.className = 'cc-move-placeholder'
+        placeHolder.style.color = 'red'
+      }
+    })
 
-// loop through all the input element and perform the logic within
-inputsDiv.forEach((div) => {
-  // get the first child of the input div element
-  let input = div.firstElementChild
-  //   get the span element of the input variable (also its first child)
-  let placeHolder = div.querySelector('span')
+    //  for each input, listen for the blur event
+    input.addEventListener('blur', (event) => {
+      //assign the placeholder element to the place holder varable decleared above
+      placeHolder = event.target.parentNode.children[1]
+      //if the input element has an empty value and losses focus
+      if (input.value == '') {
+        //change the position and color of the placeholder
+        placeHolder.className = 'cc-placeholder'
+        placeHolder.style.color = 'grey'
+      }
+    })
 
-  //   listen for events or actions(focus event)...
-  input.addEventListener('focus', () => {
-    //on focus remove the css style class "cc-placeholder" and add the "cc-move-placeholder"
-    placeHolder.className = 'cc-move-placeholder'
-  })
+    // INPUTS VALIDATION =================
+    //for each input again, listen of input change event
+    input.addEventListener('input', (event) => {
+      //get the input type
+      const inputType = event.target
+      //get the errormessage element of said input if it has one
+      const errorInput = event.target.parentNode.parentNode.children[1]
+      placeHolder = event.target.parentNode.children[1] //get the placeholder
 
-  input.addEventListener('blur', () => {
-    //  when the input is out of focus(blur), check if it has an empty value
-    if (input.value == '') {
-      // if it has empty value, add the css class "cc-placeholder"
-      placeHolder.className = 'cc-placeholder'
-      placeHolder.style.color = 'grey'
-    }
+      //check the validity of the input
+      if (inputType.type == 'text' || inputType.type == 'email') {
+        
+        if (inputType.validity.valid) {
+          //if the input is valid, change the color of the placeholder
+          placeHolder.style.color = 'grey'
+          errorInput.classList.add('d-none')
+          //also if the input type is a password input just return true, there is no error message to display
+        } else {
+          //also, if the input is not valid change the placeholder color to red
+          placeHolder.style.color = 'red'
+          showErrorMessage(inputType, errorInput) //and fire the function to show the error message.
+        }
+      }
+    })
   })
 })
 
-//============ FORM VALIDATION ===============
-
-const showErrorMessage = (inputs) => {
-  inputs.forEach((input) => {
-    if (input.type === 'text') {
-      let span = document.querySelector('.text-div')
-
-      span.children[1].style.color = 'grey'
-      userNameErrorMessage.classList.remove('d-none')
-
-      if (input.validity.valueMissing) {
-        span.children[1].style.color = 'red'
-        userNameErrorMessage.textContent = `Please enter a user name`
-      }
-    } else if (input.type === 'email') {
-      let span = document.querySelector('.email-div')
-
-      span.children[1].style.color = 'grey'
-      emailErrorMessage.classList.remove('d-none')
-
-      if (input.validity.typeMismatch) {
-        span.children[1].style.color = 'red'
-        emailErrorMessage.textContent = `Please enter a valid email address`
-      }
+// ERROR MESSAGE =================
+//error message take two aguments, the input element and the error element
+const showErrorMessage = (input, error) => {
+  //if the input is a textfield
+  if (input.type == 'text') {
+    //and if the textfield does not have a value
+    if (input.validity.valueMissing) {
+      //display this error message
+      error.textContent = `Please enter a username`
+      error.classList.remove('d-none')
     }
+  } else if (input.type == 'email') {
+    //now, if the input is an emailfield
+    if (input.validity.typeMismatch) {
+      //and if the emailfield data does not fit an email parttern
+      error.textContent = `Please enter a valid email address` //then, display this error message.
+      error.classList.remove('d-none')
+    }
+  }
+}
+
+// a funcvtion to show the password value.
+const showPassword = () => {
+  //get the icon of the input
+  const pwdInput = document.getElementById('password')
+  const icon = document.querySelector('.fa-eye')
+  //listen for a click event
+  icon.addEventListener('click', () => {
+    let type =
+      pwdInput.getAttribute('type') === 'password' ? 'text' : 'password'
+    pwdInput.setAttribute('type', type)
+
+    icon.classList.toggle('fa-eye-slash')
   })
 }
 
-// Username validation=======================
-// listen for event or action on the email element
-userName.addEventListener('input', (e) => {
-  // when a value is typed in the input field, check if it is valid
+//here we invoked the password show function.
+showPassword()
 
-  if (userName.validity.valid) {
-    //remove error message if any
-    userNameErrorMessage.classList.add('d-none')
-  } else {
-    showErrorMessage(inputs)
-  }
-})
+// ACTIVATE SUBMIT BUTTON ===============
 
-// email validation=======================
-// listen for event or action on the email element
-email.addEventListener('input', (e) => {
-  // when a value is typed in the input field, check if it is valid
+//now we want to activate the button if all the inputs are valid...
+//we listen on the form for an input event
+form.addEventListener('input', (event) => {
+  //target the inputs(username, password and email)
+  const userName = document.getElementById('name')
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
 
-  if (email.validity.valid) {
-    //remove error message if any
-    emailErrorMessage.classList.add('d-none')
-  } else {
-    showErrorMessage(inputs)
-  }
-})
-
-// password validation =====================
-// listen for event or action on the password element
-password.addEventListener('input', (e) => {
-  // when a value is typed in the input field, check if it is valid
-  let span = document.querySelector('.password-div')
-  if (password.validity.valid) {
-    //remove error message if any
-    span.children[1].style.color = 'grey'
-  } else {
-    span.children[1].style.color = 'red'
-  }
-})
-
-// Activate Button =================
-
-inputs.forEach((input) => {
-  if (input.validity.valid) {
+  //if all the inputs are valid
+  if (
+    userName.validity.valid &&
+    email.validity.valid &&
+    password.validity.valid
+  ) {
+    //we then change the state of the submit button.
     submitButton.removeAttribute('disabled')
-    submitButton.style.backgroundColor = '#286efa'
-    submitButton.style.color = '#fff'
-    console.log('all is valid!')
+    submitButton.classList.add('cc-active')
   } else {
-    console.log('something is wrong')
+    //the submit button state remains the same
+    submitButton.addAttribute = 'disabled'
+    submitButton.classList.remove('cc-active')
   }
-})
-
-form.addEventListener('submit', (e) => {
-  // if the email field is valid, we let the form submit
-  //   e.preventDefault()
-  //   inputs.forEach((input) => {
-  //     if (input.validity.valid) {
-  //       return true
-  //     } else {
-  //       showErrorMessage(inputs)
-  //     }
-  //   })
 })
